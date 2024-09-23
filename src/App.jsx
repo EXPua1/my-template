c32b3e46edbd8b2e5f916015572d9bb4edec2bea
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-09-23 20:57:14 +0300

    clean

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "modern-normalize";
+import "./index.css";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
